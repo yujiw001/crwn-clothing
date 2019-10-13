d764a19ade78065d016ec2d5b27c526a2d8ddd38
yujiw001 <wany5d1401@gmail.com>
2019-10-12 23:55:03 -0700

    added images to the menu-item and enhance the home page styles

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './homepage.styles.scss';
+import directory from '../../components/directory/directory.component'
+import Directory from '../../components/directory/directory.component';
+const HomePage = () => (
+    <div className='homepage'>
+        <Directory />
+    </div>
+);
+
+export default HomePage;
