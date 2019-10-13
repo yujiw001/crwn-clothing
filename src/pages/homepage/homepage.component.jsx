f0b05230004658b58c2f9b6e55e4552e1cf32cfb
yujiw001 <wany5d1401@gmail.com>
2019-10-13 00:30:17 -0700

    broken my homepage into smaller component, and finished styling the menu-item(adding hover effect)

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './homepage.styles.scss';
-import directory from '../../components/directory/directory.component'
+
 import Directory from '../../components/directory/directory.component';
 const HomePage = () => (
     <div className='homepage'>
