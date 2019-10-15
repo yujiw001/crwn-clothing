7d996c3a7ff5fb2f8b92c960aeed4c1a7273287d
yujiw001 <wany5d1401@gmail.com>
2019-10-14 20:48:04 -0700

    created the shooping data, and made a shop page with them

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Switch,Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-
+import ShopPage from './pages/shop/shop.component.jsx';
 const HatsPage = () => (
   <div>
     <h1>HATS PAGE</h1>
@@ -15,6 +15,7 @@ function App() {
     <Switch>
       <Route exact path='/' component={HomePage} />
       <Route exact path='/hats' component={HatsPage} />
+      <Route exact path='/shop' component={ShopPage}/>
     </Switch>
   </div>
   );
