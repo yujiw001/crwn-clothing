fb336379bd6f1c814615b55382af08ddae8f1b9c
yujiw001 <wany5d1401@gmail.com>
2019-10-18 13:47:40 -0700

    updating our app to redirect to home if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch,Route } from 'react-router-dom';
+import { Switch,Route,Redirect } from 'react-router-dom';
 import './App.css';
 import{connect} from 'react-redux';
 import HomePage from './pages/homepage/homepage.component';
@@ -69,13 +69,19 @@ class App extends React.Component {
         <Route exact path='/' component={HomePage} />
         <Route exact path='/hats' component={HatsPage} />
         <Route exact path='/shop' component={ShopPage}/>
-        <Route exact path='/signIn' component={SignInAndSignUpPage}/>
+        {/* <Route exact path='/signIn' component={SignInAndSignUpPage}/> */}
+        <Route exact path='/signIn' render={()=>this.props.currentUser? (<Redirect to='/'/>):(<SignInAndSignUpPage />)}/>
       </Switch>
     </div>
     );
   }
 }
+//重要点，这个mapStateToProps的作用是帮我们取到reducer之中的state供我们使用
+const mapStateToProps =({user})=> ({
+  currentUser:user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
--- src/redux/user/user.reducer.jsx
+++ src/redux/user/user.reducer.jsx
@@ -1,10 +1,12 @@
+import {UserActionTypes} from './user.types'
 const INITIAL_STATE={
     currentUser: null
 }
 //下面的state=INITIAL_STATE是一个ES6新语句， 当state没有值的时候会给他一个默认值initial_state
 const userReducer = (state = INITIAL_STATE,action) => {
    switch(action.type){
-        case 'SET_CURRENT_USER':
+        case UserActionTypes.SET_CURRENT_USER:
+        // case 'SET_CURRENT_USER':
             return{
                 ...state,
                 currentUser: action.payload
