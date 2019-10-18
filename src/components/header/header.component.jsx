81d1e7e439a0012b0f4d315ecc112d100138b10d
yujiw001 <wany5d1401@gmail.com>
2019-10-18 16:42:04 -0700

    added cartIcon and cartDropdown components to our application,also implemented cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,8 +4,9 @@ import {connect} from 'react-redux';
 import {auth} from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
-
-const Header = ({currentUser}) => (
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+const Header = ({currentUser,hidden}) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo' />
@@ -25,11 +26,20 @@ const Header = ({currentUser}) => (
                     SIGN IN
                 </Link>)
             }
+            <CartIcon />
         </div>
+        {
+            hidden ? null:<CartDropdown />
+        }
+        
     </div>
 );
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+// const mapStateToProps = state => ({
+//     currentUser: state.user.currentUser
+// });
+const mapStateToProps = ({user:{currentUser},cart:{hidden}}) => ({
+    currentUser,
+    hidden
 });
 //the first argument is going to be the function that allow us to access the root reducer;
 export default connect(mapStateToProps) (Header);
